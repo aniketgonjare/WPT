8aa2c01d55693a8ebe52879001deb066c31807b5
aniketgonjare <130743811+aniketgonjare@users.noreply.github.com>
2023-05-31 20:17:16 +0530

    Add files via upload

---
+++ JavaScriptAssignment/Assignment1Call.js
@@ -0,0 +1,14 @@
+const m=require("./Assignment1");
+
+
+
+
+console.log("sum :"+m.add(5,5));
+
+
+console.log("substraction"+m.substract(5,5));
+console.log("division :"+m.divide(5,5));
+console.log("square :"+m.square(5));
+console.log("multiplication"+m.multiply(5,5));
+console.log(m.sum(5,4,6,3,2,4,2,5,5));
+console.log(m.sum(5,4,6,3,2,4,2,5,5,8,7,7,7,74,74));
